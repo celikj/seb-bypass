8889a48734d6c4bc003e596ad1361f0b65d80cd4
00100001 <108012988+dei0ces@users.noreply.github.com>
2023-03-27 19:03:37 +0300

    Add files via upload

---
+++ bypass.py
@@ -0,0 +1,16 @@
+import os
+
+user_namex = os.environ['USERNAME']
+file_path = f"C:\\Users\\{user_namex}\\Documents\\Virtual Machines\\Windows 10\\windows 10.vmx"
+
+with open(file_path, "r") as file:
+    file_text = file.read()
+
+if "SMBIOS.reflectHost = “True”" in file_text:
+    print("You have run the bypass before, you do not need to run it again.")
+else:
+    with open(file_path, "a") as file:
+        file.write("\nSMBIOS.reflectHost = “True”")
+        print("bypass completed successfully.")
+
+input("Press any key to close the program...")
