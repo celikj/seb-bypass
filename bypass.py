e8d2a395a72d9f2d41e16f06211d19e61238a700
00100001 <108012988+dei0ces@users.noreply.github.com>
2023-04-17 11:53:07 +0300

    Update bypass.py
    fix

--- bypass.py
+++ bypass.py
@@ -6,7 +6,7 @@ file_path = f"C:\\Users\\{user_namex}\\Documents\\Virtual Machines\\Windows 10\\
 with open(file_path, "r") as file:
     file_text = file.read()
 
-if "SMBIOS.reflectHost = “True”" in file_text:
+if "SMBIOS.reflectHost = "True" in file_text:
     print("You have run the bypass before, you do not need to run it again.")
 else:
     with open(file_path, "a") as file:
