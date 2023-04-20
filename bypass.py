1f76727629bf6a387dcc98f646e34cd79e205394
00100001 <108012988+dei0ces@users.noreply.github.com>
2023-04-20 22:39:09 +0300

    Update bypass.py
    final fix

--- bypass.py
+++ bypass.py
@@ -6,11 +6,11 @@ file_path = f"C:\\Users\\{user_namex}\\Documents\\Virtual Machines\\Windows 10\\
 with open(file_path, "r") as file:
     file_text = file.read()
 
-if "SMBIOS.reflectHost = "True" in file_text:
+if 'SMBIOS.reflectHost = "True"'  in file_text:
     print("You have run the bypass before, you do not need to run it again.")
 else:
     with open(file_path, "a") as file:
-        file.write("\nSMBIOS.reflectHost = "True")
-        print("SEB vm detection bypassed successfully.")
+        file.write('\nSMBIOS.reflectHost = "True"')
+        print("bypass completed successfully.")
 
 input("Press any key to close the program...")
